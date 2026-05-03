6a074be8a0d56c7d75825efff9a100d77721a7eb
Alisson Lima <alissongabriel010907@gmail.com>
2026-05-03 19:53:04 -0300

    - ajusta

--- src/test/java/com/maismaes/com/br/MaismaesApplicationTests.java
+++ src/test/java/com/maismaes/com/br/MaismaesApplicationTests.java
@@ -2,8 +2,10 @@ package com.maismaes.com.br;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MaismaesApplicationTests {
 
 	@Test
