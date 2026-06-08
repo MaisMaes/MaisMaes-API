9d51f38ca4776ba9967a6282ba6c6e9d47433555
luanamarques0 <luanamarques.silv@gmail.com>
2026-06-08 20:12:38 -0300

    merge securityconfig

--- src/main/java/com/maismaes/com/br/config/SecurityConfig.java
+++ src/main/java/com/maismaes/com/br/config/SecurityConfig.java
@@ -1,9 +1,8 @@
 package com.maismaes.com.br.config;
 
-import com.maismaes.com.br.infra.security.SecurityFilter;
 import java.util.Arrays;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,6 +16,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.maismaes.com.br.infra.security.SecurityFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
@@ -69,7 +72,8 @@ public class SecurityConfig {
             "http://localhost:8081",
             "http://10.187.133.135:8081",
             "http://192.168.1.100:8081",
-            "http://192.168.137.194:8081"));
+            "http://192.168.137.194:8081",
+            "http://192.168.1.11:8081"));
     corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
     corsConfiguration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
     corsConfiguration.setAllowCredentials(true);
