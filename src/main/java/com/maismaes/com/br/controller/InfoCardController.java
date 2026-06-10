4886540a69f54c6d7c8824a5a6a7b6abec7c7d33
luanamarques0 <luanamarques.silv@gmail.com>
2026-06-09 21:08:22 -0300

    atualização infocard

--- src/main/java/com/maismaes/com/br/controller/InfoCardController.java
+++ src/main/java/com/maismaes/com/br/controller/InfoCardController.java
@@ -1,26 +1,34 @@
 package com.maismaes.com.br.controller;
 
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.maismaes.com.br.dto.request.AtualizaDto;
 import com.maismaes.com.br.dto.request.CadastroInfocardDto;
 import com.maismaes.com.br.dto.response.InfoCardResponseDto;
 import com.maismaes.com.br.entities.Perfil;
 import com.maismaes.com.br.entities.Role;
 import com.maismaes.com.br.service.InfocardService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/infocards")
